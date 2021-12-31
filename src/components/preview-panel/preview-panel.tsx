6fb6e6557432f6764438185dc4ce326a93b340f4
Yadong Zhang <zhyd007@gmail.com>
2021-12-31 15:23:51 +0800

    refactor: added preview panel styles file.

--- src/components/preview-panel/preview-panel.tsx
+++ src/components/preview-panel/preview-panel.tsx
@@ -1,47 +1,16 @@
 import "highlight.js/styles/github.css";
-import styled from "@emotion/styled";
 import { ContentState } from "draft-js";
 import { FC, useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
-import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import hljs from "highlight.js";
 import { scan } from "@zhyd1997/draftjs-to-latex";
-
-const StyledMenu = styled.div`
-  display: flex;
-  position: relative;
-  padding: 6px;
-`;
-
-/**
- * container vs wrapper
- * @see https://stackoverflow.com/a/33404137/8537000
- */
-const SourceCodeContainer = styled.div`
-  position: relative;
-  padding: 9px;
-  box-shadow: -1px -1px 13px 3px black;
-  // overflow-x: hidden;
-  overflow-y: auto;
-`;
-
-const StyledCopyButton = styled(ContentCopyIcon)`
-  color: rgba(0.68, 0.68, 0.68, 0.5);
-  position: absolute;
-  right: 1rem;
-
-  &:hover {
-    cursor: pointer;
-    transform: scale(1.25);
-  }
-`;
-
-const SourceCodeWrapper = styled.pre`
-  margin: 0;
-  white-space: pre-wrap;
-  word-wrap: break-word;
-`;
+import {
+  SourceCodeContainer,
+  SourceCodeWrapper,
+  StyledCopyButton,
+  StyledMenu,
+} from "./preview-panel.style";
 
 type PreviewPanelProps = {
   contentState: ContentState;
@@ -83,11 +52,13 @@ export const PreviewPanel: FC<PreviewPanelProps> = ({ contentState }) => {
         </Button>
       </StyledMenu>
       <SourceCodeContainer>
-        <Tooltip title={clipboardTitle} placement="top">
-          <StyledCopyButton onClick={copyToClipboard} />
-        </Tooltip>
+        {sourceCode && (
+          <Tooltip title={clipboardTitle} placement="top">
+            <StyledCopyButton onClick={copyToClipboard} />
+          </Tooltip>
+        )}
         <SourceCodeWrapper>
-          <code className="language-latex">{sourceCode || ""}</code>
+          <code className="language-latex">{sourceCode}</code>
         </SourceCodeWrapper>
       </SourceCodeContainer>
     </>

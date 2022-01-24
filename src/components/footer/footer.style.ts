478ce442fd161aa113727eeb75f81a3ab9a796c4
Yadong Zhang <zhyd007@gmail.com>
2022-01-24 15:06:11 +0800

    feat: added powered by vercel footer. (#52)

---
+++ src/components/footer/footer.style.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const FooterContainer = styled.footer`
+  width: 100%;
+`;
+
+export const VercelBanner = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;

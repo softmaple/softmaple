c44d673dde3373912c8a08a1910e715e6371b787
Yadong Zhang <zhyd007@gmail.com>
2022-01-25 10:23:05 +0800

    perf: migrate Highlight.js to Prism.js (#55)

--- src/components/preview-panel/latex-containier/light-wrapper.tsx
+++ src/components/preview-panel/latex-containier/light-wrapper.tsx
@@ -1,13 +1,27 @@
-import React, { FC } from "react";
-import styled from "@emotion/styled";
+import React, { FC, useEffect } from "react";
 import { SourceCodeWrapper } from "./latex-container.style";
-
-const Wrapper = styled(SourceCodeWrapper)``;
 
 type LightWrapperProps = {
   children: React.ReactNode;
 };
 
-export const LightWrapper: FC<LightWrapperProps> = ({ children }) => (
-  <Wrapper>{children}</Wrapper>
-);
+export const LightWrapper: FC<LightWrapperProps> = ({ children }) => {
+  useEffect(() => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href =
+      "https://cdn.jsdelivr.net/npm/prismjs@1.26.0/themes/prism.min.css";
+
+    document.head.appendChild(link);
+
+    return () => {
+      document.head.removeChild(link);
+    };
+  }, []);
+
+  return (
+    <div>
+      <SourceCodeWrapper>{children}</SourceCodeWrapper>
+    </div>
+  );
+};

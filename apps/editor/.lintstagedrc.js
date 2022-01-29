0fb66173ae0d72a4adb0e64a98b221eed5121754
Yadong Zhang <zhyd007@gmail.com>
2022-01-29 10:18:58 +0800

    fix: moved 'Layout' and 'Palette' components to packages/ui. + chore: fix next lint --fix error.

--- apps/editor/.lintstagedrc.js
+++ apps/editor/.lintstagedrc.js
@@ -1,8 +1,5 @@
 module.exports = {
   "**/*.ts?(x)": (filenames) =>
     filenames.map((file) => `prettier --write '${file}'`),
-  "**/*.ts?(x)": (filenames) =>
-    `next lint --fix --file ${filenames
-      .map((file) => file.split(process.cwd())[1])
-      .join(" --file ")}`,
+  "**/*.ts?(x)": ["eslint --fix", "git add"],
 };
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,1 +1,3 @@
 export * from "./Header";
+export * from "./Layout";
+export * from "./Palette";

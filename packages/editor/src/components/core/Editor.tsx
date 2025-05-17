a1990b47e791b38ed81852dd21dbc4f44670a604
zhyd1997 <zhyd007@gmail.com>
2025-05-17 12:25:49 +0800

    feat(packages/editor): init Lexical editor.

---
+++ packages/editor/src/components/core/Editor.tsx
@@ -0,0 +1,46 @@
+import type { FC } from "react";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import type { InitialConfigType } from "@lexical/react/LexicalComposer";
+import { Providers } from "@/components/core/Providers.tsx";
+import type { EditorThemeClasses } from "lexical";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+
+const theme: EditorThemeClasses = {
+  // Theme styling goes here
+  //...
+};
+
+// Catch any errors that occur during Lexical updates and log them
+// or throw them as needed. If you don't throw them, Lexical will
+// try to recover gracefully without losing user data.
+const onError = (error: Error) => {
+  throw error;
+};
+
+type EditorProps = unknown;
+
+export const Editor: FC<EditorProps> = () => {
+  const initConfig: InitialConfigType = {
+    namespace: "Playground",
+    theme,
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initConfig}>
+      <Providers>
+        <RichTextPlugin
+          contentEditable={
+            <ContentEditable
+              aria-placeholder={"Enter some text..."}
+              placeholder={<div>Enter some text...</div>}
+            />
+          }
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+      </Providers>
+    </LexicalComposer>
+  );
+};

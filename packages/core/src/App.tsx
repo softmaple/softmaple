c450172f12ea0111cb9c7ffc4abea53f80e02d26
zhyd1997 <zhyd007@gmail.com>
2025-05-17 11:19:55 +0800

    feat(apps/core): setup shadcn.

--- packages/core/src/App.tsx
+++ packages/core/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "@/components/ui/button.tsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,10 +18,10 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>

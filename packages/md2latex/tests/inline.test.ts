bcf18ec9cac033578325bea745393c440a20040e
zhyd1997 <zhyd007@gmail.com>
2025-05-19 19:34:39 +0800

    test(packages/md2latex): added more unit tests.

--- packages/md2latex/tests/inline.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect } from "vitest";
-import { markdownToLatex } from "../src/md2latex";
-
-describe("inline", () => {
-  const mdStr = `**bold**`;
-  const tex = markdownToLatex(mdStr);
-  expect(tex).toBe(`\\textbf{bold}`);
-});

82aa0cf2c76767d57b7c3b2838d7b95b9776b866
zhyd1997 <zhyd007@gmail.com>
2025-05-29 04:51:49 +0800

    fix(apps/web): updated import statement for Metadata type.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import { Metadata } from "next";
+import type { Metadata } from "next";
 
 import "@softmaple/ui/globals.css";
 import { Providers } from "@/components/providers";

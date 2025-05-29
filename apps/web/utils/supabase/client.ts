0f25153d4dffdb798e8e15531891eb6da05159fb
zhyd1997 <zhyd007@gmail.com>
2025-05-29 19:12:35 +0800

    refactor(apps/web): simplify Supabase client creation by removing type annotation

--- apps/web/utils/supabase/client.ts
+++ apps/web/utils/supabase/client.ts
@@ -2,7 +2,7 @@
 
 import { createBrowserClient } from "@supabase/ssr";
 
-export const createClient = (): ReturnType<typeof createBrowserClient> =>
+export const createClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,

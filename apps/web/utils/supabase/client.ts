e81e24132067d997409d5b8dd65b1524ce00349c
zhyd1997 <zhyd007@gmail.com>
2025-05-29 19:11:11 +0800

    fix(apps/web): simplified Supabase client creation by removing unused query client.

--- apps/web/utils/supabase/client.ts
+++ apps/web/utils/supabase/client.ts
@@ -1,15 +1,9 @@
 "use client";
 
 import { createBrowserClient } from "@supabase/ssr";
-import { createClient as createDClient } from "@supabase/supabase-js";
 
 export const createClient = (): ReturnType<typeof createBrowserClient> =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
-
-export const createQueryClient = createDClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-);

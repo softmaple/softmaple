7b8854f5e43e61b197af59f15c82e6b5e0d97839
Yadong (Adam) Zhang <zhyd007@gmail.com>
2025-05-29 13:25:42 +0800

    feat: setup supabase with Prisma in turborepo. (#160)

---
+++ apps/web/utils/supabase/server.ts
@@ -0,0 +1,32 @@
+import { createServerClient } from "@supabase/ssr";
+import type { CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export const createClient = async (
+  cookieStore: ReturnType<typeof cookies>,
+): Promise<ReturnType<typeof createServerClient>> => {
+  const { getAll, set } = await cookieStore;
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              set(name, value, options),
+            );
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    },
+  );
+};

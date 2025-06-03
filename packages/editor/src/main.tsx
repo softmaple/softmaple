c52aa6f9451d443e856ea5f39b3f255314b2a0c7
Yadong (Adam) Zhang <zhyd007@gmail.com>
2025-06-03 09:01:39 +0800

    fix(packages/editor): editor ui broke in `dev` mode. (#240)
    * fix(packages/editor): editor ui broke in `dev` mode.
    
    * fix(packages/editor): add PostCSS configuration by exporting from `@softmaple/ui`

--- packages/editor/src/main.tsx
+++ packages/editor/src/main.tsx
@@ -2,8 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
+import "@softmaple/ui/globals.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );

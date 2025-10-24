c8921f9e94602f31a7486744edf027c952bda85f
Yadong (Adam) Zhang <zhyd007@gmail.com>
2025-10-24 11:56:53 +0000

    chore: upgrade Next.js to v16.0.0 and React to v19.2.0
    - Upgrade Next.js from 15.3.2 to 16.0.0
    - Upgrade React and React-dom from 19.0.0 to 19.2.0
    - Update @types/react and @types/react-dom to 19.2.2
    - Migrate from 'next lint' to ESLint CLI
    - Update eslint.config.mjs to use direct imports from eslint-config-next
    - Apply Next.js codemods for v16 compatibility

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,16 +1,13 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;

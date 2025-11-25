44755d8a0833353ce508deac583ce848897f2d75
zhyd1997 <zhyd007@gmail.com>
2025-11-25 10:49:49 +0000

    chore(packages/ui): update all shadcn components to latest canary

--- packages/ui/src/components/separator.tsx
+++ packages/ui/src/components/separator.tsx
@@ -1,9 +1,9 @@
-"use client";
+"use client"
 
-import * as React from "react";
-import * as SeparatorPrimitive from "@radix-ui/react-separator";
+import * as React from "react"
+import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
-import { cn } from "@softmaple/ui/lib/utils";
+import { cn } from "@softmaple/ui/lib/utils"
 
 function Separator({
   className,
@@ -13,16 +13,16 @@ function Separator({
 }: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
   return (
     <SeparatorPrimitive.Root
-      data-slot="separator-root"
+      data-slot="separator"
       decorative={decorative}
       orientation={orientation}
       className={cn(
         "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
-        className,
+        className
       )}
       {...props}
     />
-  );
+  )
 }
 
-export { Separator };
+export { Separator }

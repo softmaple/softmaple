00cc57a043123a6dd4587fd19309a877c6c67712
zhyd1997 <zhyd007@gmail.com>
2025-11-30 13:59:36 +0000

    Revert "fix(packages/db): correct path references in prisma config and migrate script"
    This reverts commit 1ac41a4c12bc1845871bd7f4e4859c31bc9862ed.

--- packages/db/prisma/prisma.config.ts
+++ packages/db/prisma/prisma.config.ts
@@ -6,12 +6,12 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   // the main entry for your schema
-  schema: "schema.prisma",
+  schema: "prisma/schema.prisma",
   // where migrations should be generated
   // what script to run for "prisma db seed"
   migrations: {
-    path: "migrations",
-    // seed: "tsx prisma/seed.ts",
+    path: "prisma/migrations",
+    seed: "tsx prisma/seed.ts",
   },
   // The database URL
   datasource: {

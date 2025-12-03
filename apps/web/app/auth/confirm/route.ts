04f235c4a84b65d7e774aa7dc2dd465cd3f336c4
zhyd1997 <zhyd007@gmail.com>
2025-12-03 08:31:14 +0000

    fix(apps/web): update auth confirm route to use token_hash query param and EmailOtpType

--- apps/web/app/auth/confirm/route.ts
+++ apps/web/app/auth/confirm/route.ts
@@ -2,17 +2,18 @@ import { type NextRequest } from "next/server";
 
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import { type EmailOtpType } from "@supabase/supabase-js";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
-  const token_hash = searchParams.get("token");
-  const type = searchParams.get("type") as any;
+  const token_hash = searchParams.get("token_hash");
+  const type = searchParams.get("type") as EmailOtpType;
   const next = searchParams.get("next") ?? "/";
 
   // Throw error early if required parameters are missing
   if (!token_hash || !type) {
     throw new Error(
-      "Invalid request parameters. Please provide a valid token and type.",
+      "Invalid request parameters. Please provide a valid token_hash and type.",
     );
   }
 

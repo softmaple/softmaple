5d5b15b4a2de0c9800f6232398389f62cc529538
zhyd1997 <zhyd007@gmail.com>
2025-12-05 08:30:19 +0000

    fix(apps/web): change vitest environment to jsdom for React component testing

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node',
+    environment: 'jsdom',
     exclude: [
       '**/node_modules/**',
       '**/dist/**',

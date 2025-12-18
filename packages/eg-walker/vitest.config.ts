8c5bc6bb632922f3beac8a2751cb4f887f00a3d9
zhyd1997 <zhyd007@gmail.com>
2025-12-18 09:16:06 +0000

    ci(packages/eg-walker): add coverage reporting to PR comments

--- packages/eg-walker/vitest.config.ts
+++ packages/eg-walker/vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     environment: 'node',
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'json', 'html', 'json-summary'],
       include: ['src/**/*.ts'],
       exclude: ['src/test/**', 'src/**/*.test.ts', 'src/**/*.spec.ts'],
       thresholds: {
